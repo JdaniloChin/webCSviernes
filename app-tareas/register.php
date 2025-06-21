d7d4acff82a808e2b3681c0adfb61c1e6cbe7147
JdaniloChin <danilochin91@gmail.com>
2025-06-20 20:11:48 -0600

    COMMIT 11

--- app-tareas/register.php
+++ app-tareas/register.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-if($_SERVER["REGUEST_METHOD"] == "POST") {
+if($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email'];
     $password = $_POST['password'];
     $confirm_password = $_POST['confirm-password'];
